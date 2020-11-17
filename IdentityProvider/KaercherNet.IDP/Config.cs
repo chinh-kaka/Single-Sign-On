b8e21e940456703a3ab99aacd4791227864aa0bd
Kak'a Chinh <chinh.bui@orientsoftware.com>
2020-11-17 16:52:43 +0700

    implement authentication with client credentials

---
+++ IdentityProvider/KaercherNet.IDP/Config.cs
@@ -0,0 +1,33 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace KaercherNet.IDP
+{
+    public class Config
+    {
+        public static IEnumerable<ApiScope> ApiScopes()
+        {
+            return new List<ApiScope>
+            {
+                new ApiScope("kaerchernetApi", "Customer api for kaerchernet")
+            };
+        }
+
+        public static IEnumerable<Client> Clients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "kaerchernetApi" }
+                }
+            };
+        }
+    }
+}
