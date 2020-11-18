bb1ec7e0c751def3bff9da845b48b33ad1504b95
Kak'a Chinh <chinh.bui@orientsoftware.com>
2020-11-18 16:39:00 +0700

    add Kaernet idp project and ef core

---
+++ IdentityProvider/Kaercher.IDP/Quickstart/TestUsers.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Test;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace IdentityServerHost.Quickstart.UI
+{
+    public class TestUsers
+    {
+        public static List<TestUser> Users = new List<TestUser>
+        {
+             new TestUser
+             {
+                 SubjectId = "d860efca-22d9-47fd-8249-791ba61b07c7",
+                 Username = "kaka",
+                 Password = "P@ssword123",
+
+                 Claims = new List<Claim>
+                 {
+                     new Claim("given_name", "kaka"),
+                     new Claim("family_name", "chinh"),
+                 }
+             },
+             new TestUser
+             {
+                 SubjectId = "b7539694-97e7-4dfe-84da-b4256e1ff5c7",
+                 Username = "kane",
+                 Password = "P@ssword123",
+
+                 Claims = new List<Claim>
+                 {
+                     new Claim("given_name", "harry"),
+                     new Claim("family_name", "kane"),
+                 }
+             }
+         };
+    }
+}
---
+++ IdentityProvider/Kaercher.IDP/Startup.cs
@@ -0,0 +1,132 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using IdentityServerHost.Quickstart.UI;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Kaercher.IDP
+{
+    public class Startup
+    {
+        public IWebHostEnvironment Environment { get; }
+
+        public Startup(IWebHostEnvironment environment)
+        {
+            Environment = environment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // uncomment, if you want to add an MVC-based UI
+            services.AddControllersWithViews();
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+
+            string connectionString = config.GetSection("connectionString").Value;
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            var builder = services.AddIdentityServer(options =>
+            {
+                // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
+                options.EmitStaticAudienceClaim = true;
+            })
+                //.AddInMemoryIdentityResources(Config.IdentityResources)
+                //.AddInMemoryApiScopes(Config.ApiScopes)
+                //.AddInMemoryClients(Config.Clients)
+                .AddTestUsers(TestUsers.Users)
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = builder =>
+                                    builder.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = builder => builder.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly));
+                    // this enables automatic token cleanup. this is optional.
+                    options.EnableTokenCleanup = true;
+                    options.TokenCleanupInterval = 30;
+                });
+
+            // not recommended for production - you need to store your key material somewhere secure
+            builder.AddDeveloperSigningCredential();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            InitializeIdentityServerDatabase(app);
+
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // uncomment if you want to add MVC
+            app.UseStaticFiles();
+            app.UseRouting();
+            
+            app.UseIdentityServer();
+
+            // uncomment, if you want to add MVC
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+
+        private void InitializeIdentityServerDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+
+                //Seed the data
+                //Seed the clients
+                //if (!context.Clients.Any())
+                //{
+                //    foreach (var client in Config.Clients)
+                //    {
+                //        context.Clients.Add(client.ToEntity());
+                //    }
+                //    context.SaveChanges();
+                //}
+
+                ////Seed the identity resources
+                //if (!context.IdentityResources.Any())
+                //{
+                //    foreach (var resource in Config.IdentityResources)
+                //    {
+                //        context.IdentityResources.Add(resource.ToEntity());
+                //    }
+                //    context.SaveChanges();
+                //}
+
+                ////Seed api recources
+                //if (!context.ApiResources.Any())
+                //{
+                //    foreach (var resource in Config.Apis)
+                //    {
+                //        context.ApiResources.Add(resource.ToEntity());
+                //    }
+                //    context.SaveChanges();
+                //}
+            }
+        }
+    }
+}
