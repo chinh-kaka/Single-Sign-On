cf50f22f1a1dd7bed40e595ca442771424423f7d
Kak'a Chinh <chinh.bui@orientsoftware.com>
2020-11-18 11:55:34 +0700

    add swagger client and identity ui

--- IdentityProvider/KaercherNet.IDP/Config.cs
+++ IdentityProvider/KaercherNet.IDP/Config.cs
@@ -1,22 +1,84 @@
-﻿using IdentityServer4.Models;
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
 using System.Collections.Generic;
 
 namespace KaercherNet.IDP
 {
+    //public class Config
+    //{
+    //    public static List<TestUser> GetUsers()
+    //    {
+    //        return new List<TestUser>
+    //        {
+    //            new TestUser { SubjectId = "1", Username = "kaka", Password = "P@ssword123" }
+    //        };
+    //    }
+
+    //    public static IEnumerable<ApiScope> ApiScopes()
+    //    {
+    //        return new List<ApiScope>
+    //        {
+    //            new ApiScope("kaerchernetApi", "Customer api for kaerchernet")
+    //        };
+    //    }
+
+    //    public static IEnumerable<Client> Clients()
+    //    {
+    //        return new List<Client>
+    //        {
+    //            new Client
+    //            {
+    //                ClientId = "client",
+    //                AllowedGrantTypes = GrantTypes.ClientCredentials,
+    //                ClientSecrets =
+    //                {
+    //                    new Secret("secret".Sha256())
+    //                },
+    //                AllowedScopes = { "kaerchernetApi" }
+    //            }
+    //        };
+    //    }
+    //}
+
     public class Config
     {
-        public static IEnumerable<ApiScope> ApiScopes()
+        public static IEnumerable<IdentityResource> GetIdentityResources()
         {
-            return new List<ApiScope>
+            return new List<IdentityResource>
             {
-                new ApiScope("kaerchernetApi", "Customer api for kaerchernet")
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
             };
         }
 
-        public static IEnumerable<Client> Clients()
+        public static List<TestUser> GetUsers()
         {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "kaka",
+                    Password = "P@ssword123"
+                }
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetAllApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("kaerchernetApi", "Customer Api for kaerchernet")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+
             return new List<Client>
             {
+                //Client-Credential based grant type
                 new Client
                 {
                     ClientId = "client",
@@ -26,6 +88,50 @@ namespace KaercherNet.IDP
                         new Secret("secret".Sha256())
                     },
                     AllowedScopes = { "kaerchernetApi" }
+                },
+
+                //Resource Owner Password grant type
+                new Client
+                {
+                    ClientId = "ro.client",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "kaerchernetApi" }
+                },
+
+                //Implicit Flow Grant Type
+                new Client
+                {
+                    ClientId = "mvc",
+                    ClientName = "MVC Client",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+
+                    RedirectUris = { "http://localhost:5005/signin-oidc" },
+                    PostLogoutRedirectUris = { "http://localhost:5005/signout-callback-oidc"},
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    }
+                },
+
+                 //Swagger Client
+                new Client
+                {
+                    ClientId = "swaggerapiui",
+                    ClientName = "Swagger API UI",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+
+                    RedirectUris = { "http://localhost:5005/swagger/oauth2-redirect.html" },
+                    PostLogoutRedirectUris = { "http://localhost:5005/swagger"},
+
+                    AllowedScopes = { "kaerchernetApi" },
+                    AllowAccessTokensViaBrowser = true
                 }
             };
         }
--- IdentityProvider/KaercherNet.IDP/Startup.cs
+++ IdentityProvider/KaercherNet.IDP/Startup.cs
@@ -1,7 +1,14 @@
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 
 namespace KaercherNet.IDP
 {
@@ -11,20 +18,87 @@ namespace KaercherNet.IDP
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+
+            string connectionString = config.GetSection("connectionString").Value;
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
             services.AddIdentityServer()
                 .AddDeveloperSigningCredential()
-                .AddInMemoryApiScopes(Config.ApiScopes())
-                .AddInMemoryClients(Config.Clients());
+                //.AddInMemoryApiScopes(Config.ApiScopes())
+                //.AddInMemoryClients(Config.Clients())
+                .AddTestUsers(Config.GetUsers())
+                // this adds the operational data from DB (codes, tokens, consents)
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = builder =>
+                                    builder.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = builder => builder.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly));
+                    // this enables automatic token cleanup. this is optional.
+                    options.EnableTokenCleanup = true;
+                    options.TokenCleanupInterval = 30;
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            InitializeIdentityServerDatabase(app);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
             app.UseIdentityServer();
+
+            app.UseStaticFiles();
+        }
+
+        private void InitializeIdentityServerDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                //Seed the data
+                //Seed the clients
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Config.GetClients())
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                //Seed the identity resources
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Config.GetIdentityResources())
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                //Seed api recources
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in Config.GetAllApiResources())
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
